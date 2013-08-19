7c9d8757b3fa785d62cb126df0d0442044c56258
misatoalexandre <misatotina.alexandre@gmail.com>
2013-08-19 15:50:09 -0400

    Initial Commit

---
+++ BooksTests/BooksTests.m
@@ -0,0 +1,34 @@
+//
+//  BooksTests.m
+//  BooksTests
+//
+//  Created by Misato Tina Alexandre on 8/19/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BooksTests : XCTestCase
+
+@end
+
+@implementation BooksTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
