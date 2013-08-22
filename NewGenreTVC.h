8bba894b140929127c33fff69690f8da5136032c
misatoalexandre <misatotina.alexandre@gmail.com>
2013-08-22 12:21:09 -0400

    added new files for NewGenre: addition of a user’s new genre view controller.

---
+++ NewGenreTVC.h
@@ -0,0 +1,13 @@
+//
+//  NewGenreTVC.h
+//  Books
+//
+//  Created by Misato Tina Alexandre on 8/22/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewGenreTVC : UITableViewController
+
+@end
